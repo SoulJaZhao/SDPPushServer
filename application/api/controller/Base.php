812c6f587e08ae71f6976c3b4c02ba0c48ccfafe
zhaolong01 <zhaolong01@shengpay.com>
2017-09-13 16:59:54 +0800

    1.添加登录标识

---
+++ application/api/controller/Base.php
@@ -0,0 +1,11 @@
+<?php
+//公共组件
+namespace app\api\controller;
+
+use think\Controller;
+
+class Base extends Controller 
+{
+	
+}
+?>
---
+++ application/api/controller/Login.php
@@ -0,0 +1,15 @@
+<?php
+//登录模块
+namespace app\api\controller;
+
+use think\Controller;
+use app\api\controller\Base;
+
+class Login extends Base 
+{
+	//登录方法
+	public function index() {
+		echo 'Login()';
+	}
+}
+?>
